12566bdd8027ce28bd8978edcfa4241d86539efc
Balint Seeber <balint256@users.noreply.github.com>
2012-04-08 23:08:06 +0000

    Added GNU GPL licence

--- BorIP/stdafx.cpp
+++ BorIP/stdafx.cpp
@@ -1,8 +1,5 @@
-
 // stdafx.cpp : source file that includes just the standard includes
 // BorIP.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
-
-
